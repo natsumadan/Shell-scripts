2671cea873efae640ceb273841342d6ef57cce41
natsumadan <madan.mohit209@gmail.com>
2021-03-05 16:25:58 +1000

    first commit

---
+++ greeting.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+FIRST_NAME=Bob
+FAVORITE_COLOR=blue
+
+
+echo Hi $FIRST_NAME, your fovourite color is $FAVORITE_COLOR
---
+++ hello.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+# Display a gretting 
+
+
+echo Hello, World.
+echo Mars is Red.
